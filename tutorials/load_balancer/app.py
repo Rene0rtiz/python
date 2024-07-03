1e92b435123ee44a6cfeda0aaf7138af85f83b6a
rene <>
2024-07-03 16:31:47 -0700

    update

--- tutorials/load_balancer/app.py
+++ tutorials/load_balancer/app.py
@@ -4,9 +4,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def sample():
     return f'This is the {os.environ["APP"]} application.'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
